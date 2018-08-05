71a9e066aa718588aed9eb8904ea68540d347b2f
mirai <xiaotututuer@gmail.com>
2018-08-05 11:26:12 +0800

    添加智能提示和User路由

--- flask_learn.py
+++ flask_learn.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # type: Flask
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/user', methods=['POST'])
+def hello_user():
+    return 'Hello User!'
 
 
 if __name__ == '__main__':

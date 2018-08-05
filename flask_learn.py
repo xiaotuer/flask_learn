748f25b9962a7299ca11b51e16f792fcd885a78c
mirai <xiaotututuer@gmail.com>
2018-08-05 16:05:28 +0800

    反向路由

--- flask_learn.py
+++ flask_learn.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)  # type: Flask
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    content = "小鸟唧唧jijij"
+    return render_template("index.html", content=content)
 
 # 指定方式
 @app.route('/user', methods=['POST'])
@@ -22,6 +23,11 @@ def query_user():
     id = request.args.get('id')
     return 'query user: ' + id
 
+
+# 反向路由
+@app.route('/query_url')
+def query_url():
+    return 'query_url: ' + url_for('query_user')
 
 if __name__ == '__main__':
     app.run()

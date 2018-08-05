e73b7b509ac14b1527a34af93a749561e98df127
mirai <xiaotututuer@gmail.com>
2018-08-05 15:16:00 +0800

    添加了两种传递参数的方法

--- flask_learn.py
+++ flask_learn.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)  # type: Flask
 
@@ -7,9 +7,20 @@ app = Flask(__name__)  # type: Flask
 def hello_world():
     return 'Hello World!'
 
+# 指定方式
 @app.route('/user', methods=['POST'])
 def hello_user():
     return 'Hello User!'
+
+# 传递参数
+@app.route('/users/<id>')
+def user_id(id):
+    return 'hello: user ' + id
+
+@app.route('/query_user')
+def query_user():
+    id = request.args.get('id')
+    return 'query user: ' + id
 
 
 if __name__ == '__main__':

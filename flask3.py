b33057a3129a83fa26c1e3eec994f4f263ae3d0f
mirai <xiaotututuer@gmail.com>
2018-08-05 21:33:10 +0800

    flask表单

--- flask3.py
+++ flask3.py
@@ -1,34 +1,52 @@
 # -*- encoding:utf-8 -*-
 from flask import Flask, render_template, flash,request
-
+from wtforms import Form, TextField, PasswordField, validators, StringField
 app = Flask(__name__) # type: Flask
 app.secret_key = '123'
 
+
+class LoginForm(Form):
+    username = StringField("username", [validators.DataRequired()])
+    password = PasswordField("username", [validators.DataRequired()])
+
 @app.route('/')
 def hello_world():
+    myForm = LoginForm(request.form)
     flash("hello 小袅袅")
-    return render_template("login.html")
+    return render_template("login.html", form=myForm)
 
 
 @app.route('/login', methods=['POST'])
 def login():
+    myForm = LoginForm(request.form)
     form = request.form
     username = form.get('username')
     password = form.get('password')
     if not username:
         flash("please input username")
-        return render_template("login.html")
+        return render_template("login.html", form=myForm)
     if not password:
         flash("please input password")
-        return render_template("login.html")
-    if username=="123" and password == "321":
+        return render_template("login.html", form=myForm)
+    if myForm.username.data=="123" and myForm.password.data == "321" and myForm.validate():
         flash("login success")
-        return render_template("login.html")
+        return render_template("login.html", form=myForm)
     else:
         flash("username or password is wrong")
-        return render_template("login.html")
+        return render_template("login.html", form=myForm)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
+@app.route('/users/<user_id>')
+def users(user_id):
+    if int(user_id) == 1:
+        return render_template("user.html")
+    else:
+        return  render_template("404.html")
 
 
 
